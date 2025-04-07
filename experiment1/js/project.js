56f891a671b72ade578d534faaf3e6af576167bf
IanLiu2603 <ian.liu2603@gmail.com>
2025-04-07 14:53:41 -0700

    Experiment 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,115 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  team: [
+    "Warriors",
+    "Lakers",
+    "Kings",
+    "Clippers",
+    "Nuggets",
+    "Thunder",
+    "Rockets",
+    "Suns",
+    "Spurs",
+    "Jazz",
+    "Timberwolves",
+    "Mavericks",
+    "Trail Blazers",
+    "Pelicans",
+    "Grizzlies",
+    "Cavaliers",
+    "Celtics",
+    "Knicks",
+    "Pacers",
+    "Pistons",
+    "Magic",
+    "Heat",
+    "Bucks",
+    "Bulls",
+    "Hawks",
+    "Raptors",
+    "Nets",
+    "76ers",
+    "Hornets",
+    "Wizards",
+  ],
+  age: ["19", "18", "22", "20"],
+  height: [
+    "5 foot 10 inches",
+    "6 foot 3 inches",
+    "6 foot 7 inches",
+    "6 foot 4 inches",
+    "6 foot 9 inches",
+    "7 foot",
+    "6 foot 6 inches",
+  ],
+  weight: ["185", "225", "190", "200", "215", "180"],
+  shooting: [
+    "Stephen Curry",
+    "Michael Porter Jr.",
+    "Taj Gibson",
+    "Scoot Henderson",
+    "Shake Milton",
+    "Oscar Tshiebwe",
+  ],
+  defense: [
+    "Draymond Green",
+    "Rudy Gobert",
+    "Luka Doncic",
+    "Jamychal Green",
+    "Amen Thompson",
+  ],
+  handles: [
+    "Kyrie Irving",
+    "Rudy Gobert",
+    "Jared McCain",
+    "Stephen Curry",
+    "Giannis Antetokounmpo",
+  ],
+  finishing: [
+    "Klay Thompson",
+    "Victor Oladipo",
+    "Anthony Lamb",
+    "Kyrie Irving",
+    "Taurean Prince",
+  ],
+  iq: [
+    "Nikola Jokic",
+    "Draymond Green",
+    "PJ Tucker",
+    "Bones Hyland",
+    "Buddy Hield",
+  ],
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `With the first pick in the 2052 Draft, the $team select Ian Liu Jr.
+At age $age, Ian Liu Jr. is listed at $height, $weight.
+After his run in the NCAA March Madness Tournament, scouts compared his shooting to $shooting
+His former college coaches compared his defensive prowess to $defense and his iq to $iq
+After watching him at the combine, we believe he dribbles like $handles and finishes around the rim like $finishing`;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+// STUDENTS: You don't need to edit code below this line.
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
